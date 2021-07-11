53ff3c38f17ae40d987eea78b319dd7527f9d01a
syumai <syumai@gmail.com>
2021-07-11 23:24:45 +0900

    add toI func to print one's complement

--- bitoperation/sum.go
+++ bitoperation/sum.go
@@ -5,16 +5,27 @@ import (
 )
 
 func main() {
-	const zero uint8 = 0b1111_1111
-	var sum = sumOfOnesComplement(zero, zero) // 0 + 0 => 0
-	fmt.Printf("%08b + %08b = %08b\n", zero, zero, sum)
+	var a, b, sum uint8
+	a = 0b1111_1110                 // -1
+	b = 0b0000_0011                 // 3
+	sum = sumOfOnesComplement(a, b) // 2
+	fmt.Printf("%d + %d = %d\n", toI(a), toI(b), toI(sum))
+
+	a = 0b1000_0000                 // -127
+	b = 0b0001_1011                 // 27
+	sum = sumOfOnesComplement(a, b) // -100
+	fmt.Printf("%d + %d = %d\n", toI(a), toI(b), toI(sum))
 }
 
 func sumOfOnesComplement(a, b uint8) uint8 {
 	var sum uint16
 	sum = uint16(a) + uint16(b)
-	if sum>>8 == 1 { // checks overflow
-		sum += 100
+	return uint8(sum + sum>>8) // add overflowed bit and trim to 8bits
+}
+
+func toI(c uint8) int8 {
+	if c>>7 == 0 {
+		return int8(c)
 	}
-	return uint8(sum) // trim overflowed bit
+	return -1 * int8(^c)
 }
