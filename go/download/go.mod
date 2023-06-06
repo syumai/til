8dd129ab1ea41d913140d42d10819ffa7dac2f20
syumai <syumai@gmail.com>
2023-06-06 23:40:02 +0900

    add file download example

---
+++ go/download/go.mod
@@ -0,0 +1,3 @@
+module github.com/syumai/til/go/download
+
+go 1.20
---
+++ go/download/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"strings"
+)
+
+const (
+	fileName    = "ファイル の 名前.txt"
+	fileContent = "ファイル の 中身"
+	port        = ":8111"
+)
+
+func main() {
+	// 両方指定
+	http.HandleFunc("/file1", func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Content-Disposition", fmt.Sprintf(`attachment;
+filename=%q;
+filename*=utf-8''%s`, fileName, url.PathEscape(fileName)))
+		io.Copy(w, strings.NewReader(fileContent))
+	})
+	// filenameだけ指定
+	http.HandleFunc("/file2", func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Content-Disposition", fmt.Sprintf(`attachment;
+filename=%q`, fileName))
+		io.Copy(w, strings.NewReader("ファイル の 中身"))
+	})
+	// filename*=だけ指定
+	http.HandleFunc("/file3", func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Content-Disposition", fmt.Sprintf(`attachment;
+filename*=utf-8''%s`, url.PathEscape(fileName)))
+		io.Copy(w, strings.NewReader("ファイル の 中身"))
+	})
+	fmt.Printf("Listening: %s\n", port)
+	http.ListenAndServe(port, nil)
+}
