35dfd90de1f1dd18e823f3f3b88c1199f57b07f3
syumai <syumai@gmail.com>
2024-04-21 20:37:38 +0900

    implement author server

--- sqlc/sqlc-tutorial/go.mod
+++ sqlc/sqlc-tutorial/go.mod
@@ -1,3 +1,22 @@
 module github.com/syumai/til/sqlc/sqlc-tutorial
 
 go 1.22.2
+
+require (
+	github.com/labstack/echo v3.3.10+incompatible
+	google.golang.org/appengine v1.6.8
+)
+
+require (
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/labstack/gommon v0.4.2 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.2 // indirect
+	golang.org/x/crypto v0.22.0 // indirect
+	golang.org/x/net v0.24.0 // indirect
+	golang.org/x/sys v0.19.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	google.golang.org/protobuf v1.26.0 // indirect
+)
--- sqlc/sqlc-tutorial/main.go
+++ sqlc/sqlc-tutorial/main.go
@@ -1,5 +1,81 @@
 package main
 
-func main() {
+import (
+	"database/sql"
+	"fmt"
+	"net/http"
+	"os"
+	"strconv"
 
+	"github.com/labstack/echo"
+	"github.com/syumai/til/sqlc/sqlc-tutorial/model"
+)
+
+type server struct {
+	queries *model.Queries
+}
+
+func (s *server) ListAuthors(c echo.Context) error {
+	authors, err := s.queries.ListAuthors(c.Request().Context())
+	if err != nil {
+		return err
+	}
+	return c.JSON(http.StatusOK, authors)
+}
+
+func (s *server) GetAuthor(c echo.Context) error {
+	authorID, err := strconv.ParseInt(c.Param("id"), 10, 64)
+	if err != nil {
+		return err
+	}
+	author, err := s.queries.GetAuthor(c.Request().Context(), authorID)
+	if err != nil {
+		return err
+	}
+	return c.JSON(http.StatusOK, author)
+}
+
+func (s *server) CreateAuthor(c echo.Context) error {
+	params := new(model.CreateAuthorParams)
+	if err := c.Bind(params); err != nil {
+		return err
+	}
+	author, err := s.queries.CreateAuthor(c.Request().Context(), *params)
+	if err != nil {
+		return err
+	}
+	return c.JSON(http.StatusOK, author)
+}
+
+func newHandler(s *server) http.Handler {
+	e := echo.New()
+	e.GET("/authors", s.ListAuthors)
+	e.GET("/authors/:id", s.GetAuthor)
+	e.POST("/authors", s.CreateAuthor)
+	return e
+}
+
+func run() error {
+	db, err := sql.Open("sqlite3", "file:db.sqlite")
+	if err != nil {
+		return err
+	}
+
+	queries := model.New(db)
+	s := &server{queries: queries}
+	handler := newHandler(s)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "9876"
+	}
+	fmt.Printf("Listening on port %s\n", port)
+	return http.ListenAndServe(fmt.Sprintf(":%s", port), handler)
+}
+
+func main() {
+	if err := run(); err != nil {
+		fmt.Fprintf(os.Stderr, "err: %v\n", err)
+		os.Exit(1)
+	}
 }
