eed9871047cf9e66b7dc29074d42490e1d52e1a3
syumai <syumai@gmail.com>
2024-04-21 19:36:19 +0900

    rename tutorial package to model

--- sqlc/sqlc-tutorial/model/models.go
+++ sqlc/sqlc-tutorial/model/models.go
@@ -2,7 +2,7 @@
 // versions:
 //   sqlc v1.26.0
 
-package tutorial
+package model
 
 import (
 	"database/sql"
--- sqlc/sqlc-tutorial/model/query.sql.go
+++ sqlc/sqlc-tutorial/model/query.sql.go
@@ -3,7 +3,7 @@
 //   sqlc v1.26.0
 // source: query.sql
 
-package tutorial
+package model
 
 import (
 	"context"
