43486d817ba8463329d677d4213f8f370bbfe8e7
syumai <syumai@gmail.com>
2025-01-06 11:44:55 +0900

    add failure.Message to failure.Wrap example

--- go/errors/failure_example/wrap/main.go
+++ go/errors/failure_example/wrap/main.go
@@ -14,7 +14,7 @@ func A() error {
 func B() error {
 	err := C()
 	if err != nil {
-		return failure.Wrap(err)
+		return failure.Wrap(err, failure.Message("error wrapped in b"))
 	}
 	return nil
 }
@@ -26,17 +26,18 @@ func C() error {
 func main() {
 	err := A()
 	if err != nil {
-		// main.B: error happened
+		// main.B: error wrapped in b: error happened
 		fmt.Printf("%v\n", err)
 
 		// [main.B] /Users/syumai/go/src/github.com/syumai/til/go/errors/failure_example/wrap/main.go:17
-		//    *errors.errorString("error happened")
+		//     message("error wrapped in b")
+		//     *errors.errorString("error happened")
 		// [CallStack]
-		//    [main.B] /Users/syumai/go/src/github.com/syumai/til/go/errors/failure_example/wrap/main.go:17
-		//    [main.A] /Users/syumai/go/src/github.com/syumai/til/go/errors/failure_example/wrap/main.go:11
-		//    [main.main] /Users/syumai/go/src/github.com/syumai/til/go/errors/failure_example/wrap/main.go:27
-		//    [runtime.main] /opt/homebrew/Cellar/go/1.19.5/libexec/src/runtime/proc.go:250
-		//    [runtime.goexit] /opt/homebrew/Cellar/go/1.19.5/libexec/src/runtime/asm_arm64.s:1172
+		//     [main.B] /Users/syumai/go/src/github.com/syumai/til/go/errors/failure_example/wrap/main.go:17
+		//     [main.A] /Users/syumai/go/src/github.com/syumai/til/go/errors/failure_example/wrap/main.go:11
+		//     [main.main] /Users/syumai/go/src/github.com/syumai/til/go/errors/failure_example/wrap/main.go:27
+		//     [runtime.main] /opt/homebrew/Cellar/go/1.23.4/libexec/src/runtime/proc.go:272
+		//     [runtime.goexit] /opt/homebrew/Cellar/go/1.23.4/libexec/src/runtime/asm_arm64.s:1223
 		fmt.Printf("%+v\n", err)
 	}
 }
